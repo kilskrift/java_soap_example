a99d44015b4d139de92d289b63c9adb53b839b0e
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2014-12-09 14:59:42 +0100

    added eclipse project w/example from http://stackoverflow.com/questions/15948927/working-soap-client-example

---
+++ src/java_soap_example/SOAPClientSAAJ.java
@@ -0,0 +1,68 @@
+package java_soap_example;
+
+import javax.xml.soap.*;
+
+public class SOAPClientSAAJ {
+
+    public static void main(String args[]) throws Exception {
+        // Create SOAP Connection
+        SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+        SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+
+        // Send SOAP Message to SOAP Server
+        String url = "http://ws.cdyne.com/emailverify/Emailvernotestemail.asmx";
+        SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(), url);
+
+        // print SOAP Response
+        System.out.print("Response SOAP Message:");
+        soapResponse.writeTo(System.out);
+
+        soapConnection.close();
+    }
+
+    private static SOAPMessage createSOAPRequest() throws Exception {
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = messageFactory.createMessage();
+        SOAPPart soapPart = soapMessage.getSOAPPart();
+
+        String serverURI = "http://ws.cdyne.com/";
+
+        // SOAP Envelope
+        SOAPEnvelope envelope = soapPart.getEnvelope();
+        envelope.addNamespaceDeclaration("example", serverURI);
+
+        /*
+        Constructed SOAP Request Message:
+        <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:example="http://ws.cdyne.com/">
+            <SOAP-ENV:Header/>
+            <SOAP-ENV:Body>
+                <example:VerifyEmail>
+                    <example:email>mutantninja@gmail.com</example:email>
+                    <example:LicenseKey>123</example:LicenseKey>
+                </example:VerifyEmail>
+            </SOAP-ENV:Body>
+        </SOAP-ENV:Envelope>
+         */
+
+        // SOAP Body
+        SOAPBody soapBody = envelope.getBody();
+        SOAPElement soapBodyElem = soapBody.addChildElement("VerifyEmail", "example");
+        SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("email", "example");
+        soapBodyElem1.addTextNode("mutantninja@gmail.com");
+        SOAPElement soapBodyElem2 = soapBodyElem.addChildElement("LicenseKey", "example");
+        soapBodyElem2.addTextNode("123");
+
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", serverURI  + "VerifyEmail");
+
+        soapMessage.saveChanges();
+
+        /* Print the request message */
+        System.out.print("Request SOAP Message:");
+        soapMessage.writeTo(System.out);
+        System.out.println();
+
+        return soapMessage;
+    }
+
+}
